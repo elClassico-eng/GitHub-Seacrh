8f2b941da43995b1a49a1d8f12337f9cdaae9d35
Danya <daniilerkesov122@gmail.com>
2024-05-17 18:44:21 +0300

    Work with designe

--- src/Components/HomeBanner.jsx
+++ src/Components/HomeBanner.jsx
@@ -2,6 +2,21 @@ import React from "react";
 
 export const HomeBanner = () => {
     return (
-        <div className="relative h-screen w-full bg-gradient-to-r from-violet-500 to-fuchsia-500"></div>
+        <div className="flex relative h-screen w-full ">
+            <div className="absolute top-0 left-0 w-full h-full z-10 bg-gradient-to-b from-black/10 to-black/80"></div>
+            <img
+                className="absolute top-0 left-0 w-full h-full z-0 object-cover"
+                src="https://images.unsplash.com/photo-1622547748225-3fc4abd2cca0?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="background image"
+            />
+            <div className="flex flex-col justify-center items-center gap-y-24  absolute top-1/2 left-0 ml-80 z-20">
+                <h1 className="text-6xl text-white text-center">
+                    Find the same repository from GitHub
+                </h1>
+                <button className="w-[270px] h-[65px] bg-white rounded-full text-xl border-black cursor-pointer">
+                    Try now
+                </button>
+            </div>
+        </div>
     );
 };
--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -6,7 +6,7 @@ export const Navigation = () => {
     return (
         <nav className="flex justify-center items-center overflow-x-auto h-24 bg-black text-white px-10 py-5 ">
             <div className="flex justify-start w-6/12">
-                <h3 className="font-bold text-xl">GitHub Search</h3>
+                <h3 className="font-normal text-4xl">GitHub Search</h3>
             </div>
 
             <div className="flex justify-end w-6/12">
